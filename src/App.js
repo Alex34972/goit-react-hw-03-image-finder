be30787759488a4c0fe79bf1f37a7b4bb23ba249
Alex34972 <alex_hifi@ukr.net>
2021-08-04 19:34:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <></>;
+import React from "react";
+import { Component } from "react";
+import Searchbar from "./components/searchbar";
+import Button from "./components/button";
+import s from "./App.module.css";
+import ImageGallery from "./components/imageGallery";
+export default class App extends Component {
+  state = {
+    imagesName: "",
+  };
+  handleFormSubmit = (imagesName) => {
+    this.setState({ imagesName });
+  };
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <div className={s.App}>
+          <ImageGallery imagesName={this.state.imagesName} />
+        </div>
+        <Button />
+      </>
+    );
+  }
 }
-
-export default App;
