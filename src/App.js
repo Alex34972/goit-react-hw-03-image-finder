26e38267d22c4081286d69ef087d279ca368930f
Alex34972 <alex_hifi@ukr.net>
2021-08-05 13:24:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import Searchbar from "./components/searchbar";
 import Button from "./components/button";
 import s from "./App.module.css";
 import ImageGallery from "./components/imageGallery";
+import imageAPI from "./services/image-api";
 export default class App extends Component {
   state = {
     imagesName: "",
+    page: 1,
   };
   handleFormSubmit = (imagesName) => {
     this.setState({ imagesName });
   };
+  handleNextPage = () => {
+    this.setState(({ page }) => {
+      console.log(page);
+      return { page: page + 1 };
+    });
+  };
   render() {
+    const { imagesName } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleFormSubmit} />
         <div className={s.App}>
-          <ImageGallery imagesName={this.state.imagesName} />
+          <ImageGallery imagesName={imagesName} />
         </div>
-        <Button />
+        <Button onMore={this.handleNextPage} />
       </>
     );
   }
