a0227e6943a6db5e4df5ee2c59583fa9ee90eb5f
Alex34972 <alex_hifi@ukr.net>
2021-08-05 22:16:17 +0300

    Update ImageGalleryItem.js

--- src/components/imageGalleryItem/ImageGalleryItem.js
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+
 import s from "./ImageGalleryItem.module.css";
-const ImageGalleryItem = ({ images, modalContent }) => {
-  return (
-    <>
-      {images.map((image) => (
-        <li className={s.ImageGalleryItem} key={image.id}>
-          <img
-            src={image.webformatURL}
-            alt={image.tags}
-            className={s.ImageGalleryItem__image}
-            onClick={() => this.modalContent(image.id)}
-          />
-        </li>
-      ))}
-    </>
-  );
-};
+
+export default class ImageGalleryItem extends Component {
+  modalContent = (id) => {
+    this.props.onItemClick(id);
+  };
+  render() {
+    const { id, webformatURL } = this.props;
+    return (
+      <img
+        src={webformatURL}
+        alt=""
+        className={s.ImageGalleryItem_image}
+        onClick={() => this.modalContent(id)}
+      />
+    );
+  }
+}
 ImageGalleryItem.propTypes = {
   onItemClick: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+  webformatURL: PropTypes.string.isRequired,
 };
-export default ImageGalleryItem;
