d131dc1cb388810cfc51b6ee301585a93e953c36
Alex34972 <alex_hifi@ukr.net>
2021-08-05 22:16:09 +0300

    Update ImageGallery.js

--- src/components/imageGallery/ImageGallery.js
+++ src/components/imageGallery/ImageGallery.js
@@ -1,27 +1,36 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import ImageGalleryItem from "../imageGalleryItem";
 import s from "./ImageGallery.module.css";
+import ImageGalleryItem from "../imageGalleryItem";
 
-export default class ImagesInfo extends Component {
-  state = {
-    images: [],
-  };
-
+export default class ImageGallery extends Component {
   handleOpenModal = (e) => {
     if (e.target !== e.currentTarget) {
       this.props.onClick();
     }
   };
-  render() {
-    const { images } = this.props;
 
+  render() {
+    const { images, onItemClick } = this.props;
     return (
-      <>
-        <ul className={s.ImageGallery} onClick={this.handleOpenModal}>
-          {images && <ImageGalleryItem images={images} />}
-        </ul>
-      </>
+      <ul className={s.ImageGallery} onClick={this.handleOpenModal}>
+        {images &&
+          images.map((image) => (
+            <li key={image.id} className={s.ImageGalleryItem}>
+              <ImageGalleryItem {...image} onItemClick={onItemClick} />
+            </li>
+          ))}
+      </ul>
     );
   }
 }
+ImageGallery.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  onItemClick: PropTypes.func.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
