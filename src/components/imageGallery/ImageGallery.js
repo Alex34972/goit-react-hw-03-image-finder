30f8c03a5ee452e81aae6a8be39dc866900257f4
Alex34972 <alex_hifi@ukr.net>
2021-08-05 17:43:26 +0300

    Update ImageGallery.js

--- src/components/imageGallery/ImageGallery.js
+++ src/components/imageGallery/ImageGallery.js
@@ -1,38 +1,23 @@
 import { Component } from "react";
-import { toast } from "react-toastify";
+
 import ImageGalleryItem from "../imageGalleryItem";
-import imageAPI from "../../services/image-api";
 import s from "./ImageGallery.module.css";
-import LoaderComponent from "../loader/LoaderComponent";
 
 export default class ImagesInfo extends Component {
   state = {
-    images: null,
-    error: null,
-    loading: true,
+    images: [],
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevProps.imagesName;
-    const nextName = this.props.imagesName;
-    if (prevName !== nextName) {
-      imageAPI
-        .fetchImages(nextName)
-        .then((images) => this.setState({ images: images.hits }))
-        .catch((error) => toast(error))
-        .finally(() => this.setState({ loading: false }));
-    }
-  }
   handleOpenModal = (e) => {
     if (e.target !== e.currentTarget) {
       this.props.onClick();
     }
   };
   render() {
-    const { images, loading } = this.state;
+    const { images } = this.props;
+
     return (
       <>
-        {loading && <LoaderComponent />}
         <ul className={s.ImageGallery} onClick={this.handleOpenModal}>
           {images && <ImageGalleryItem images={images} />}
         </ul>
