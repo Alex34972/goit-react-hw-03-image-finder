510eb6b2dda83ee63ff52bdc4c362de571ff76a2
Alex34972 <alex_hifi@ukr.net>
2021-08-05 13:24:40 +0300

    Update Button.js

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,9 @@
 import React from "react";
+
 import s from "./Button.module.css";
-const Button = () => {
+const Button = ({ onMore }) => {
   return (
-    <button type="submit" className={s.Button}>
+    <button type="button" className={s.Button} onClick={onMore}>
       Load more
     </button>
   );
